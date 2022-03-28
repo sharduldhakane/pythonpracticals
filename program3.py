c27eb570778afbc764baa8accf0aef9f44e40a01
root-rvl <91834746+root-rvl@users.noreply.github.com>
2022-03-28 18:29:21 +0530

    Add files via upload

---
+++ program3.py
@@ -0,0 +1,7 @@
+# Author: Riddhish V. Lichade
+# Python program to find the square root
+import math
+print("This is a python program to find the square root of a number")
+n=int(input("Enter a number: "))
+sq=math.sqrt(n)
+print("The square root of",n,"is",sq)
