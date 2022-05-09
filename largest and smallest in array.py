ff17b5c2f17d62ab5adc4a430c3c711ee6dd066e
root-rvl <91834746+root-rvl@users.noreply.github.com>
2022-05-09 16:20:27 +0530

    Add files via upload

---
+++ largest and smallest in array.py
@@ -0,0 +1,16 @@
+# Author: Riddhish V. Lichade
+# Program to print largest and smallest element in an array
+
+
+l=eval(input("Enter a list: "))
+length=len(l)
+min=l[0]
+max=l[0]
+for i in l:
+    if i<min:
+        min=i
+    if i>max:
+        max=i
+
+print("The largest element in array is ",max)
+print("The smallest element in array is ",min)
