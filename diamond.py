3bd2de5f603d78f5d06b192d48657a54dec92bdd
root-rvl <91834746+root-rvl@users.noreply.github.com>
2022-06-06 12:02:50 +0530

    Add files via upload

--- diamond.py
+++ diamond.py
@@ -1,4 +1,29 @@
 # Author: Riddhish V. Lichade
 # Program to print a diamond pattern
+def Diamond(rows):
+    n = 0
+    for i in range(1, rows + 1):
+        # loop to print spaces
+        for j in range(1, (rows - i) + 1):
+            print(end=" ")
 
-n=int(input("Enter number of rows(should be odd number): "))
+        while n != (2 * i - 1):
+            print("*", end="")
+            n = n + 1
+        n = 0
+        print()
+
+    k = 1
+    n = 1
+    for i in range(1, rows):
+        for j in range(1, k + 1):
+            print(end=" ")
+        k = k + 1
+        while n <= (2 * (rows - i) - 1):
+            print("*", end="")
+            n = n + 1
+        n = 1
+        print()
+
+rows = 5
+Diamond(rows)
