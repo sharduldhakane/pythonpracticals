cafbf1ea22b0f2098149b8a25928f49705c4f5bc
root-rvl <91834746+root-rvl@users.noreply.github.com>
2022-06-14 08:43:58 +0530

    Delete ascending and descending.py

--- ascending and descending.py
+++
@@ -1,7 +0,0 @@
-# Author: Riddhish V. Lichade
-# Program to Sort elements in ascending and descending order
-
-l=eval(input("Enter a list: "))
-l.sort()
-print("Array in ascending order is: ",l)
-print("Array in descending order is: ",l[::-1])
