10953f2e052645f15b0f11ea2ec157885b68d89c
root-rvl <91834746+root-rvl@users.noreply.github.com>
2022-06-14 08:45:17 +0530

    Delete celcius2Fahrenheit.py

--- celcius2Fahrenheit.py
+++
@@ -1,6 +0,0 @@
-# Author: Riddhish V. Lichade
-# Python program to convert celcius to Fahreinheit
-print("This is a python program that converts Celcius to Fahreinheit")
-cel=int(input("Enter the temperature in Celcius: "))
-fah=cel*9/5+32
-print(cel,"Celcius = ",fah,"Fahrenheit")
