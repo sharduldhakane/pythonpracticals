d823d8c5e23a4da816ea3e808886d65fdcb0d114
root-rvl <91834746+root-rvl@users.noreply.github.com>
2022-06-15 08:45:42 +0530

    Delete swap2Variables.py

--- swap2Variables.py
+++
@@ -1,8 +0,0 @@
-# Author: Riddhish V. Lichade
-# Python program to swap two variables
-print("This python program swaps to variables")
-a=int(input("Enter value of a: "))
-b=int(input("Enter value of b: "))
-a,b=b,a
-print("After swapping:")
-print("a =",a,"b =",b)
