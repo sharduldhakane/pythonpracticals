2289f1b8a7ed39beae600580fc12caadd79a9e59
root-rvl <91834746+root-rvl@users.noreply.github.com>
2022-06-15 14:55:59 +0530

    Update length using recursion.py

--- practical 6/length using recursion.py
+++ practical 6/length using recursion.py
@@ -5,14 +5,15 @@ from sys import stdin, stdout, setrecursionlimit
 def inlist(): return list(map(str, stdin.readline().strip().split()))
 
 def lengthOfList(l):
-    length += 1
+    global length
     if l:
+        length+=1
         lengthOfList(l[1::])
-
 
 print("This is a python program to find length of list using recursion ")
 print("-------------------------------------------------------------------------------------\n")
 print("Enter the elements of list seperated by space: ")
 l = inlist()
-global length=0
+length = 0
+lengthOfList(l)
 print("Length of the given list is ", length)
